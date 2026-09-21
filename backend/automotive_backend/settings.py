a7481a7d68323543342c59e03bd61b9c9f28d1df
Fred Njau <njaufredrick0@gmail.com>
2026-09-21 03:02:05 +0300

    fix(email): support SSL port 465 for Gmail SMTP (Render)

--- backend/automotive_backend/settings.py
+++ backend/automotive_backend/settings.py
@@ -230,6 +230,7 @@ EMAIL_BACKEND = os.environ.get(
 EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
 EMAIL_PORT = int(os.environ.get('EMAIL_PORT', '587'))
 EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True').lower() == 'true'
+EMAIL_USE_SSL = os.environ.get('EMAIL_USE_SSL', 'False').lower() == 'true'
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 DEFAULT_FROM_EMAIL = os.environ.get(
